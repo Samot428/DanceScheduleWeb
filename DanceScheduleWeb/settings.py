84755f51a67aa6e974d64af6192c5fca2945ffc8
Samot428 <tomasfantomastomi@gmail.com>
2026-02-02 15:24:17 +0100

    setting csrftoken updated

--- DanceScheduleWeb/settings.py
+++ DanceScheduleWeb/settings.py
@@ -22,6 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-_ezgsm1$x43$aqwc)s&16-ja$s52pk$%!+#7@gi2n8zp97_yn&'
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.railway.app",
+]
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
