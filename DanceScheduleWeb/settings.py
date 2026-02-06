de07c703ca92da72ce2320ad5d563bac26d884c1
Samot428 <tomasfantomastomi@gmail.com>
2026-02-06 14:54:49 +0100

    updated static files rooting

--- DanceScheduleWeb/settings.py
+++ DanceScheduleWeb/settings.py
@@ -137,7 +137,7 @@ CHANNEL_LAYERS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'sheet/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     BASE_DIR / "static",
